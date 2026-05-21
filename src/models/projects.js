57b3dfb60b4e6607be055e83bbc571e1a404f1b2
Donovan <djamespoland@gmail.com>
2026-05-20 20:54:45 -0600

    middleware and error handling

--- src/models/projects.js
+++ src/models/projects.js
@@ -19,4 +19,23 @@ const getAllProjects = async() => {
     return result.rows;
 }
 
-export {getAllProjects};
+const getUpcomingProjects = async (number_of_projects) =>{
+    const query = `
+        SELECT
+            proj.project_id,
+            proj.title,
+            proj.description,
+            proj.date,
+            proj.location,
+            proj.organization_id,
+            org.org_name AS organization_name
+        FROM public.project proj
+        JOIN public.organization org ON p.organization_id = org.organization_id
+        WHERE proj.project_datetime >= CURRENT_DATE
+        ORDER BY proj.project_datetime ASC
+        LIMIT $1;
+    `;
+}
+
+
+export {getAllProjects, getUpcomingProjects};
